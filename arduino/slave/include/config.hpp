c2bcfd30142356795e49ad8a2e832f1eaaabadd8
Rizal Anggoro <gnoogler4@gmail.com>
2024-02-18 05:32:14 +0700

    refactor code structure

--- arduino/slave/include/config.hpp
+++ arduino/slave/include/config.hpp
@@ -1,16 +1,16 @@
 #ifndef config_hpp
 #define config_hpp
 
-// #define LED_COUNT 77 // matrix
-#define LED_COUNT 50  // desk
+#define LED_COUNT 77  // matrix
+// #define LED_COUNT 50  // desk
 
-// #define LED_PIN 9  // matrix
+#define LED_PIN 9  // matrix
 // #define LED_PIN D5  // terrace
-#define LED_PIN D5  // desk
+// #define LED_PIN D5  // desk
 
 // device type
-// #define LM
+#define LM
 
-#define USE_ESP_NOW
+// #define USE_ESP_NOW
 
 #endif
